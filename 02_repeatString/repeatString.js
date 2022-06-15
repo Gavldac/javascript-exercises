a085c88c81f56e154a5fa91fffc958fe4091c172
Edwin Casady <edwin.casady@gmail.com>
2022-06-15 12:08:30 -0700

    Implements a solution to the 02-repeatString exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(str, repeat) {
+    if ( repeat < 0) return "ERROR";
+    let newStr = "";
+    while (repeat > 0){
+        newStr = newStr + str;
+        repeat--;
+    } 
+    return newStr;
 };
 
 // Do not edit below this line
