5ba8cba96d69d92919c4fc4b7d2c7428d2abf993
Edwin Casady <edwin.casady@gmail.com>
2022-06-15 22:38:25 -0700

    Implements solution for 04-removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(argsList, ...removeThese) {
+    const removeArgs = Array.from(removeThese);
+    let argsArray = Array.from(argsList);
+    for(const arg of removeArgs){
+        for (let i = 0; i < argsArray.length; i++){
+            if(arg === argsArray[i]){
+                argsArray.splice(i,1);
+            }
+        }
+    }
+    return argsArray;
 
 };
 
