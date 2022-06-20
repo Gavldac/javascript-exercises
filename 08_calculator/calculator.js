e076c70f5d457d509aa83e1d959aa6d52efea051
Edwin Casady <edwin.casady@gmail.com>
2022-06-20 11:28:18 -0700

    Implements solutions for exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function (num1 = 0, num2 = 0) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function (num1 = 0, num2 = 0) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function (nums) {
+  let sumOf = 0;
+  for (let num of nums) {
+    sumOf += num;
+  }
+  return sumOf;
 };
 
-const multiply = function() {
-
+const multiply = function (nums) {
+  let answer = 1;
+  nums.forEach(element => {
+    answer *= element;
+  });
+  return answer;
 };
 
-const power = function() {
-	
+const power = function (num, expo) {
+  let answer = 1;
+  for (let i = 0; i < expo; i++) {
+    answer *= num;
+  }
+  return answer;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  let answer = 1;
+  for (let i = 0; i < num; ){
+    answer *= ++i;
+  }
+  return answer;
 };
 
 // Do not edit below this line
