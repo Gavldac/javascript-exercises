43b93099356854da5ce0d6ba14c08946bb501443
Edwin Casady <edwin.casady@gmail.com>
2022-06-19 23:26:28 -0700

    Implements a solution to exercise 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,6 @@
-const leapYears = function() {
-
+const leapYears = function(yearNum) {
+    return (yearNum % 4 == 0 && yearNum % 100 !=0) ? true : 
+        (yearNum % 400 == 0) ? true : false;
 };
 
 // Do not edit below this line
